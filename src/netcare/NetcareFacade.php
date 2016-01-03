c4dccdf8c07eada22b2bc354a5c9b62c5bcddefb
Brian Kestenholz <briankesten@yahoo.com>
2016-01-02 23:10:22 -0600

    added facade

---
+++ src/netcare/NetcareFacade.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bk
+ * Date: 1/2/2016
+ * Time: 11:03 PM
+ */
+
+
+namespace Illuminate\Support\Facades;
+
+class NetcareFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'usd';
+    }
+
+}
+
+
+
+
+
+
